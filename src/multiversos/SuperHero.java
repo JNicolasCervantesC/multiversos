32762632dc05743b37e1adb72b1f899492b20544
JNicolasCervantesC <jncervantesc@udistrital.edu.co>
2024-03-16 23:38:55 -0500

    Multiversos app

---
+++ src/multiversos/SuperHero.java
@@ -0,0 +1,14 @@
+package multiversos;
+
+public class SuperHero extends Persona {
+
+    String heroName;
+    String powers[];
+    String franquicia;
+    Persona persona;
+
+    public SuperHero(String heroName, Persona persona) {
+        this.heroName = heroName;
+        this.persona = persona;
+    }
+}
